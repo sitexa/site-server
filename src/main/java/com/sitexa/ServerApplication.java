a9b3adac2db001b2d7d01ff52a10856c1b95ac5e
xnpeng <xnpeng@hotmail.com>
2018-11-12 20:03:13 +0800

    init

--- src/main/java/com/sitexa/ServerApplication.java
+++ src/main/java/com/sitexa/ServerApplication.java
@@ -3,10 +3,6 @@ package com.sitexa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * created by CaiBaoHong at 2018/4/17 14:15<br>
- *
- */
 @SpringBootApplication
 public class ServerApplication {
 
